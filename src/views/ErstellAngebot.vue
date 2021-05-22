b21c676e3a2e744cfed4053d3fac9582712ab48c
Umut Guengoer <Umsko@outlook.com>
2021-05-22 16:11:10 +0200

    Erstellen eines Angebotes wird jetzt als alert im Json format ausgeben. Am CSS muss noch gearbeitet werden.

--- src/views/ErstellAngebot.vue
+++ src/views/ErstellAngebot.vue
@@ -1,29 +1,67 @@
 <template>
   <div>
     <h2>Angebot erstellen</h2>
-    <p>Bilder</p>
-    <b-img
-      v-bind:src="placeholder"
-      height="165"
-      width="215"
-      align="left"
-      rounded=""
-      alt="Rounded image"
-    />
-    <!-- Plain mode -->
-    <b-form-file multiple v-model="files" class="mt-3 ml-5" plain></b-form-file>
-    <b-input-group>
-      <label>Verkaufsart:</label>
+    <b-container class="bv-example-row" @submit="onSubmit">
+      <p>Bilder</p>
       <br />
-      <select v-model="art">
-        <option v-for="art in option" :key="art">{{ art }}</option>
-      </select>
-      <label>Kategorie:</label>
-      <br />
-      <select v-model="cat">
-        <option v-for="cat in category" :key="cat">{{ cat }}</option>
-      </select>
-    </b-input-group>
+      <b-img
+        v-bind:src="placeholder"
+        height="165"
+        width="215"
+        align="left"
+        rounded=""
+        alt="Rounded image"
+      />
+      <b-form class="form-group">
+        <!-- Plain mode -->
+        <b-form-file
+          multiple
+          v-model="files"
+          class="mt-3 ml-5"
+          plain
+        ></b-form-file>
+        <label>Verkaufsart:</label>
+        <br />
+        <select v-model="angebot.art">
+          <option v-for="art in option" :key="art">{{ art }}</option>
+        </select>
+        <label>Kategorie:</label>
+        <br />
+        <select v-model="angebot.cat">
+          <option v-for="cat in category" :key="cat">{{ cat }}</option>
+        </select>
+        <b-form-group
+          id="input-group-1"
+          label="Betrag:"
+          label-for="input-1"
+          v-if="angebot.art !== 'Tausch'"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="angebot.betrag"
+            v-if="angebot.art !== 'Tausch'"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-2" label="Title:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="angebot.title"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <label>Beschreibung:</label>
+        <b-form-textarea
+          id="textarea"
+          v-model="angebot.text"
+          placeholder="Beschreibung"
+        >
+        </b-form-textarea>
+        <b-button type="submit" class="btn btn-dark btn-lg btn-block"
+          >Erstellen</b-button
+        >
+      </b-form>
+    </b-container>
   </div>
 </template>
 
@@ -32,18 +70,24 @@ import placeholder from "../assets/profile.png";
 export default {
   data() {
     return {
+      angebot: {
+        art: "",
+        cat: "",
+        betrag: "",
+        title: "",
+        text: "",
+      },
       files: [],
       placeholder: placeholder,
       option: ["Verkauf", "Tausch"],
-      art: "",
-      cat: "",
       category: ["Literatur", "Mikrocontroller", "Datenträger", "Hardware"],
     };
   },
   methods: {
-    OnClick(event) {
+    onSubmit: function (event) {
       event.preventDefault();
-      alert("Button got clicked");
+      alert(JSON.stringify(this.angebot));
+      console.log(JSON.stringify(this.angebot));
     },
   },
 };
@@ -54,5 +98,8 @@ export default {
   width: 15%;
   margin-left: auto;
   margin-right: auto;
+}
+p {
+  float: left;
 }
 </style>
