84dc4944b94b379937f971b9d7621dfad60bf05d
Umut Guengoer <Umsko@outlook.com>
2021-05-22 15:28:01 +0200

    Bilder upload & Verkaufsart & Kategorie added to Angebot erstellen

--- src/views/ErstellAngebot.vue
+++ src/views/ErstellAngebot.vue
@@ -1,18 +1,45 @@
 <template>
   <div>
-    <h2>Meine Angebote</h2>
-    <b-button
-      href="/erstellangebot"
-      type="forward"
-      class="btn btn-dark btn-lg btn-block"
-    >
-      Angebot erstellen
-    </b-button>
+    <h2>Angebot erstellen</h2>
+    <p>Bilder</p>
+    <b-img
+      v-bind:src="placeholder"
+      height="165"
+      width="215"
+      align="left"
+      rounded=""
+      alt="Rounded image"
+    />
+    <!-- Plain mode -->
+    <b-form-file multiple v-model="files" class="mt-3 ml-5" plain></b-form-file>
+    <b-input-group>
+      <label>Verkaufsart:</label>
+      <br />
+      <select v-model="art">
+        <option v-for="art in option" :key="art">{{ art }}</option>
+      </select>
+      <label>Kategorie:</label>
+      <br />
+      <select v-model="cat">
+        <option v-for="cat in category" :key="cat">{{ cat }}</option>
+      </select>
+    </b-input-group>
   </div>
 </template>
 
 <script>
+import placeholder from "../assets/profile.png";
 export default {
+  data() {
+    return {
+      files: [],
+      placeholder: placeholder,
+      option: ["Verkauf", "Tausch"],
+      art: "",
+      cat: "",
+      category: ["Literatur", "Mikrocontroller", "Datenträger", "Hardware"],
+    };
+  },
   methods: {
     OnClick(event) {
       event.preventDefault();
